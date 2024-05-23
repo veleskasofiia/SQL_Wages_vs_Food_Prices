b6c1115db7b145084510ab4d29bcde06b3f78bda
Sofiia Veleska <136385960+veleskasofiia@users.noreply.github.com>
2024-05-24 00:30:13 +0200

    Update 3_SQL_Minimal_Food_Price_Growth.sql
    Improve SQL query

--- 3_SQL_Minimal_Food_Price_Growth.sql
+++ 3_SQL_Minimal_Food_Price_Growth.sql
@@ -1,15 +1,46 @@
-SELECT * FROM t_sofiia_veleska_sql_primary_final
-
+/*
 SELECT
-    a.category_code,
-    a.year_from AS current_year,
-    b.year_from AS previous_year,
-    a.average_price AS price_current_year,
-    b.average_price AS price_previous_year,
-    ROUND((AVG(b.average_price) / AVG(a.average_price) * 100)- 100 , 2) AS persentage_change
-FROM t_Sofiia_Veleska_SQL_primary_final a
-JOIN  t_Sofiia_Veleska_SQL_primary_final b
-	ON a.category_code = b.category_code
-	AND a.year_from -1 = b.year_from
-GROUP BY a.category_code
+    tpf1.category_code,
+    tpf1.current_year,
+    ROUND(b.price_current_year, 2) AS price_current_year,
+    ROUND(tpf1.price_previous_year, 2) AS price_previous_year,
+    tpf1.previous_year,
+    ROUND(((tpf2.price_current_year - tpf1.price_previous_year) / tpf1.price_previous_year * 100), 2) AS percentage_increase
+FROM
+    (SELECT
+        category_code,
+        year_from AS current_year,
+        average_price AS price_previous_year,
+        year_from - 1 AS previous_year
+    FROM t_Sofiia_Veleska_SQL_primary_final
+    GROUP BY category_code,
+			year_from) tpf1
+JOIN
+    (SELECT
+        category_code,
+        year_from,
+        AVG(average_price) AS price_current_year
+    FROM t_Sofiia_Veleska_SQL_primary_final
+    GROUP BY category_code,
+			year_from) tpf2
+ON tpf1.category_code = tpf2.category_code
+    AND tpf1.previous_year = tpf2.year_from
+	AND ROUND(((tpf2.price_current_year - tpf1.price_previous_year) / tpf1.price_previous_year * 100), 2) LIKE '-%'
+ORDER BY percentage_increase ASC;
+*/
+
+-- finalni code
+SELECT
+    tpf1.category_code,
+    tpf1.year_from AS current_year,
+    tpf2.year_from AS previous_year,
+    tpf1.average_price AS price_current_year,
+    tpf2.average_price AS price_previous_year,
+    ROUND((AVG(tpf2.average_price) / AVG(tpf1.average_price) * 100)- 100 , 2) AS persentage_change
+FROM t_Sofiia_Veleska_SQL_primary_final tpf1
+JOIN  t_Sofiia_Veleska_SQL_primary_final tpf2
+	ON tpf1.category_code = tpf2.category_code
+	AND tpf1.year_from -1 = tpf2.year_from
+GROUP BY tpf1.category_code
 ORDER BY persentage_change;
+
