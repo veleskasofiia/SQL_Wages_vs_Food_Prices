839b662833f61ac1e3e61a9b722a9aa0d47f2617
Sofiia Veleska <136385960+veleskasofiia@users.noreply.github.com>
2024-05-24 00:32:43 +0200

    Update 4_SQL_Wage_vs._Food_Inflation.sql
    Improve SQL query

--- 4_SQL_Wage_vs._Food_Inflation.sql
+++ 4_SQL_Wage_vs._Food_Inflation.sql
@@ -1,33 +1,28 @@
--- Progress
-
-SELECT * FROM t_Sofiia_Veleska_SQL_primary_final
-
--- Product
+-- product
 
 SELECT 
-	ROUND(AVG(a.average_price),2) AS price_current_year,
-	ROUND(AVG(b.average_price),2) AS price_previous_year,
-	ROUND(AVG(b.average_price) / AVG(a.average_price) * 100 - 100, 2) AS percentage_increase_prod
-FROM t_Sofiia_Veleska_SQL_primary_final a
-LEFT JOIN t_Sofiia_Veleska_SQL_primary_final b
-	ON a.year_from -1 = b.year_from 
-	WHERE b.average_price IS NOT NULL
-	AND a.category_code = b.category_code
-GROUP BY a.year_from;
+	ROUND(AVG(tpf1.average_price),2) AS price_current_year,
+	ROUND(AVG(tpf2.average_price),2) AS price_previous_year,
+	ROUND(AVG(tpf2.average_price) / AVG(tpf1.average_price) * 100 - 100, 2) AS percentage_increase_prod
+FROM t_Sofiia_Veleska_SQL_primary_final tpf1
+LEFT JOIN t_Sofiia_Veleska_SQL_primary_final tpf2
+	ON tpf1.year_from -1 = tpf2.year_from 
+	WHERE tpf2.average_price IS NOT NULL
+	AND tpf1.category_code = tpf2.category_code
+GROUP BY tpf1.year_from;
 
- -- Salary
-
+ -- salary
 SELECT 
-	a.payroll_year,
-	b.payroll_year AS previous_year,
-	ROUND(AVG(a.average_salary),2) AS avg_salary,
-	ROUND(AVG(b.average_salary),2) AS prev_avg_salary,
-	ROUND(AVG(b.average_salary) / AVG(a.average_salary) * 100 - 100, 2) AS percentage_increase_sal
-FROM t_Sofiia_Veleska_SQL_primary_final a
-LEFT JOIN t_Sofiia_Veleska_SQL_primary_final b
-	ON a.payroll_year -1 = b.payroll_year 
-	WHERE b.payroll_year IS NOT NULL
-GROUP BY a.payroll_year;
+	tpf1.payroll_year,
+	tpf2.payroll_year AS previous_year,
+	ROUND(AVG(tpf1.average_salary),2) AS avg_salary,
+	ROUND(AVG(tpf2.average_salary),2) AS prev_avg_salary,
+	ROUND(AVG(tpf2.average_salary) / AVG(tpf1.average_salary) * 100 - 100, 2) AS percentage_increase_sal
+FROM t_Sofiia_Veleska_SQL_primary_final tpf1
+LEFT JOIN t_Sofiia_Veleska_SQL_primary_final tpf2
+	ON tpf1.payroll_year -1 = tpf2.payroll_year 
+	WHERE tpf2.payroll_year IS NOT NULL
+GROUP BY tpf1.payroll_year;
 
 
 
@@ -35,20 +30,20 @@ GROUP BY a.payroll_year;
 
 
 SELECT 
-    a.year_from, 
-    b.year_from AS previous_year,
-    ROUND(AVG(a.average_price),2) AS current_year_price,
-    ROUND(AVG(b.average_price),2) AS previous_year_price,
-    ROUND((AVG(b.average_price) / AVG(a.average_price) * 100) - 100, 2 )AS percentage_p,
-    a.payroll_year,
-    b.payroll_year AS previous_year,
-    ROUND(AVG(a.average_salary),2) AS current_year_salary,
-    ROUND(AVG(b.average_salary),2) AS previous_year_salary,
-    ROUND((AVG(b.average_salary) / AVG(a.average_salary) * 100) - 100, 2) AS percentage_increase_s,
-    ROUND((AVG(b.average_price) / AVG(a.average_price) * 100) - 100, 2 ) - ROUND((AVG(b.average_salary) / AVG(a.average_salary) * 100) - 100, 2) AS differences
-FROM t_sofiia_veleska_sql_primary_final a
-JOIN t_sofiia_veleska_sql_primary_final b 
-    ON a.year_from - 1 = b.payroll_year 
-    AND a.category_code = b.code
-GROUP BY a.year_from;
+    tpf1.year_from, 
+    tpf2.year_from AS previous_year,
+    ROUND(AVG(tpf1.average_price),2) AS current_year_price,
+    ROUND(AVG(tpf2.average_price),2) AS previous_year_price,
+    ROUND((AVG(tpf2.average_price) / AVG(tpf1.average_price) * 100) - 100, 2 )AS percentage_p,
+    tpf1.payroll_year,
+    tpf2.payroll_year AS previous_year,
+    ROUND(AVG(tpf1.average_salary),2) AS current_year_salary,
+    ROUND(AVG(tpf2.average_salary),2) AS previous_year_salary,
+    ROUND((AVG(tpf2.average_salary) / AVG(tpf1.average_salary) * 100) - 100, 2) AS percentage_increase_s,
+    ROUND((AVG(tpf2.average_price) / AVG(tpf1.average_price) * 100) - 100, 2 ) - ROUND((AVG(tpf2.average_salary) / AVG(tpf1.average_salary) * 100) - 100, 2) AS differences
+FROM t_sofiia_veleska_sql_primary_final tpf1
+JOIN t_sofiia_veleska_sql_primary_final tpf2 
+    ON tpf1.year_from - 1 = tpf2.payroll_year 
+    AND tpf1.category_code = tpf2.code
+GROUP BY tpf1.year_from;
 		
