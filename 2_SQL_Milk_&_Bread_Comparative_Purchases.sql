acf18beec8cfe405af80a35450ce24adeea7e2d8
Sofiia Veleska <136385960+veleskasofiia@users.noreply.github.com>
2024-05-24 00:25:50 +0200

    Update 2_SQL_Milk_&_Bread_Comparative_Purchases.sql
    Improve SQL query

--- 2_SQL_Milk_&_Bread_Comparative_Purchases.sql
+++ 2_SQL_Milk_&_Bread_Comparative_Purchases.sql
@@ -2,13 +2,14 @@
 -- '114201' - Mléko polotučné pasterované
 
 SELECT 
-    b.category_code,
-    a.payroll_year,
-   	ROUND(AVG(b.average_price),2) AS product_price,
-    ROUND(AVG(a.average_salary),2) AS average_salary,
-    ROUND((a.average_salary / b.average_price), 2) AS milc_bread_can_buy
-FROM t_Sofiia_Veleska_SQL_primary_final a
-JOIN t_Sofiia_Veleska_SQL_primary_final b
-WHERE b.category_code IN ('111301','114201') 
-GROUP BY b.category_code, 
-		a.payroll_year ASC;		
+    tpf2.category_code,
+    tpf1.payroll_year,
+   	ROUND(AVG(tpf2.average_price),2) AS product_price,
+    ROUND(AVG(tpf1.average_salary),2) AS average_salary,
+    ROUND((tpf1.average_salary / tpf2.average_price), 2) AS milc_bread_can_buy
+FROM t_Sofiia_Veleska_SQL_primary_final tpf1
+JOIN t_Sofiia_Veleska_SQL_primary_final tpf2
+WHERE tpf2.category_code IN ('111301','114201')
+GROUP BY tpf2.category_code, 
+		tpf1.payroll_year ASC;		
+	
