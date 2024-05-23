37bca548496dac0f42182a85525154b131d6d3b6
Sofiia Veleska <136385960+veleskasofiia@users.noreply.github.com>
2024-05-24 00:24:45 +0200

    Update 1_SQL_Industry_Wage_Trends.sql
    Improve SQL query

--- 1_SQL_Industry_Wage_Trends.sql
+++ 1_SQL_Industry_Wage_Trends.sql
@@ -1,18 +1,18 @@
 SELECT 
-    a.industry_branch_code,
-    a.payroll_year AS current_year,
-    ROUND(AVG(b.average_salary),2) AS salary_current_year,
-    ROUND(AVG(a.average_salary),2) AS salary_previous_year,
-    b.payroll_year AS previous_year,
-    ROUND(((b.average_salary * 100) / a.average_salary) - 100, 2) AS salary_difference
-FROM t_Sofiia_Veleska_SQL_primary_final a
-JOIN t_Sofiia_Veleska_SQL_primary_final  b 
-ON a.industry_branch_code = b.industry_branch_code
-    AND a.payroll_year -1 = b.payroll_year 
-    WHERE a.average_salary <= b.average_salary
-GROUP BY a.industry_branch_code
+    tpf1.industry_branch_code,
+    tpf1.payroll_year AS current_year,
+    ROUND(AVG(tpf2.average_salary),2) AS salary_current_year,
+    ROUND(AVG(tpf1.average_salary),2) AS salary_previous_year,
+    tpf2.payroll_year AS previous_year,
+    ROUND(((tpf2.average_salary * 100) / tpf1.average_salary) - 100, 2) AS salary_difference
+FROM t_Sofiia_Veleska_SQL_primary_final tpf1
+JOIN t_Sofiia_Veleska_SQL_primary_final  tpf2 
+ON tpf1.industry_branch_code = tpf2.industry_branch_code
+    AND tpf1.payroll_year -1 = tpf2.payroll_year 
+    WHERE tpf1.average_salary <= tpf2.average_salary
+GROUP BY tpf1.industry_branch_code
 ORDER BY 
 	salary_difference DESC,
-    a.industry_branch_code,
-    a.payroll_year DESC;
+    tpf1.industry_branch_code,
+    tpf1.payroll_year DESC;
 
